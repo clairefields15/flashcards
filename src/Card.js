3cd7bffbe193a8695c2ccc3d5dc6ac0855cf06b2
Claire Fields <clairefields15@gmail.com>
2021-05-06 11:48:47 -0600

    Pass all card tests

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+class Card {
+    constructor(id, question) {
+        this.id = id;
+        this.question = question;
+        this.answers = [ 'object', 'array', 'function' ]
+        this.correctAnswer = 'object'
+    }
+}
+
+module.exports = Card;
